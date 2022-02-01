8e0ab9f3c32ec6720a03a55a2082495859287542
Shuib Abdillahi <71759511+ShuibCodes@users.noreply.github.com>
2022-02-01 16:49:42 +0000

    attempted to fix navbar mobile menu bug (minor)

--- src/components/header.js
+++ src/components/header.js
@@ -56,7 +56,7 @@ function Header() {
 							</Link>
 						</div>
 						<div className="-mr-2 -my-2 md:hidden">
-							<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+							<Popover.Button className="">
 								<span className="sr-only">Open menu</span>
 								<MenuIcon className="h-6 w-6 relative left-28 " aria-hidden="true" />
 							</Popover.Button>
@@ -156,7 +156,7 @@ function Header() {
 										</Link>
 									</div>
 									<div className="-mr-2">
-										<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+										<Popover.Button className="">
 											<span className="sr-only">Close menu</span>
 											<XIcon className="h-6 w-6" aria-hidden="true" />
 										</Popover.Button>
