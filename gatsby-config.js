8748b41c0531df771f12cf05a03584fe9606c688
Shuib <71759511+ShuibCodes@users.noreply.github.com>
2022-03-05 11:45:36 +0000

    changed tab title

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Starter Tailwind`,
-		description: `Gatsby starter styled with Tailwind`,
-		author: `@taylorbryant`,
+		title: `Master Supreme Clean`,
+		description: `Master Supreme Clean`,
+		author: `ShuaybCodes`,
 	},
 	plugins: [
 		`gatsby-plugin-smoothscroll`,
